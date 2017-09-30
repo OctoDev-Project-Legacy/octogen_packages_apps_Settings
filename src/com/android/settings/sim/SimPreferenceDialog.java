f01de63bdb3ff0dfbe1d2c2ee38fce2574dcff52
BeansTown106 <nbenis106@gmail.com>
2017-09-30 10:42:18 +0800

    add activity to launch headsup settings fragment
    Change-Id: I6587a1bf7e132e10759b5a9d858ddfd3ed866b22
    Signed-off-by: XrTV <binder_z@mail.ru>

--- src/com/android/settings/sim/SimPreferenceDialog.java
+++ src/com/android/settings/sim/SimPreferenceDialog.java
@@ -99,24 +99,18 @@ public class SimPreferenceDialog extends Activity {
 
         EditText nameText = (EditText)mDialogLayout.findViewById(R.id.sim_name);
         nameText.setText(savedInstanceState.getString(SIM_NAME));
-<<<<<<< HEAD
         int textLength = nameText.getText().length();
         nameText.setSelection(textLength);
-=======
         Utils.setEditTextCursorPosition(nameText);
->>>>>>> 361df46834... Settings: Correctly align cursor in Settings
     }
 
     private void createEditDialog(Bundle bundle) {
         final Resources res = mContext.getResources();
         EditText nameText = (EditText)mDialogLayout.findViewById(R.id.sim_name);
         nameText.setText(mSubInfoRecord.getDisplayName());
-<<<<<<< HEAD
         int textLength = nameText.getText().length();
         nameText.setSelection(textLength);
-=======
         Utils.setEditTextCursorPosition(nameText);
->>>>>>> 361df46834... Settings: Correctly align cursor in Settings
         final Spinner tintSpinner = (Spinner) mDialogLayout.findViewById(R.id.spinner);
         SelectColorAdapter adapter = new SelectColorAdapter(mContext,
                 R.layout.settings_color_picker_item, mColorStrings);
